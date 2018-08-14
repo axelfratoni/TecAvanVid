1fd9c08ff12c6e42a8502a8c15157340756614c2
EricHorvat <eric.nahuel.horvat@gmail.com>
2018-08-13 21:47:20 -0300

    Read BitBuffer functions, write test

--- Unity Project Test/Assets/Libs/BitBuffer.cs
+++ Unity Project Test/Assets/Libs/BitBuffer.cs
@@ -1,19 +1,21 @@
 ﻿using System;
 using System.IO;
+using UnityEngine.Analytics;
 
 namespace Libs
 {
     public class BitBuffer
     {
+        private bool mode;
         private long bits;
         private int currentBitCount;
         private MemoryStream buffer;
 
-        public BitBuffer(long bits, int currentBitCount, int buffer_length)
+        public BitBuffer(int buffer_length)
         {
-            this.bits = bits;
-            this.currentBitCount = currentBitCount;
-            buffer = new MemoryStream(capacity:1024);
+            bits = 0;
+            currentBitCount = 0;
+            buffer = new MemoryStream(capacity:buffer_length);
         }
 
         public void writeBit(bool toWriteBool){
@@ -23,10 +25,26 @@ namespace Libs
             writeBuffer();
         }
 
+        public bool readBit(){
+            bool value = (bits & (1 << currentBitCount)) > 0;
+            currentBitCount++;
+            //writeBuffer();
+            return value
+        }
+
         public void writeBits(long value, int bitCount){
             for (int i = 0; i < bitCount; i++)
             {
-                writeBit((value & (1 << bitCount)) > 0);
+                writeBit((value & (1 << i)) > 0);
+            }
+        }
+
+        public long readBits(int bitCount)
+        {
+            long ans = 0;
+            for (int i = 0; i < bitCount; i++)
+            {
+                ans |= readBit() ? 1L : 0L << i;
             }
         }
 
@@ -34,8 +52,16 @@ namespace Libs
             writeBits(value-min,GetBitsRequired(max-min));
         }
 
+        public int readInt(int min, int max){
+            return (int) (min + readBits(GetBitsRequired(max-min)));
+        }
+
         public void writeFloat(float value, float min, float max, float step){
             writeInt((int)Math.Round((value-min)/step),(int)Math.Round((min)/step),(int)Math.Round((max)/step));
+        } 
+
+        public float readFloat(float min, float max, float step){
+            return min + step * readInt((int) Math.Round((min) / step), (int) Math.Round((max) / step));
         } 
         
         private static int GetBitsRequired(long value) {
@@ -65,14 +91,34 @@ namespace Libs
                 currentBitCount -= 32;
             }
         }
+        
+        private void fillBuffer(long value, int bitCount){
+            //TODO
+            if (currentBitCount >= 32) {
+                if (buffer.Position + 4 > buffer.Capacity) {
+                    throw new InvalidOperationException("write buffer overflow");
+                }
+                int word = (int) bits;
+                byte a = (byte) (word);
+                byte b = (byte) (word >> 8);
+                byte c = (byte) (word >> 16);
+                byte d = (byte) (word >> 24);
+                buffer.WriteByte(d);
+                buffer.WriteByte(c);
+                buffer.WriteByte(b);
+                buffer.WriteByte(a);
+                bits >>= 32;
+                currentBitCount -= 32;
+            }
+        }
 
-        public void flash(){
+        public void flush(){
             writeBits(0,32 - currentBitCount);
             writeBuffer();
         }
 
-        public MemoryStream getBuffer(){
-            return buffer;
+        public byte[] getBuffer(){
+            return buffer.GetBuffer();
         }
     }
 }
--- Unity Project Test/Assets/Scripts/UDPClient.cs
+++ Unity Project Test/Assets/Scripts/UDPClient.cs
@@ -2,12 +2,14 @@
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using Libs;
 using UnityEngine;
 
 public class UDPClient : MonoBehaviour {
 
     Socket sending_socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
     IPEndPoint sending_end_point = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 11000);
+    BitBuffer bitBuffer = new BitBuffer(1024);
 
     // Use this for initialization
 	void Start ()
@@ -29,11 +31,16 @@ public class UDPClient : MonoBehaviour {
         byte[] send_buffer = Encoding.ASCII.GetBytes(clientMessage);
         try
         {
-            sending_socket.SendTo(send_buffer, sending_end_point);
+            bitBuffer.writeInt(422,350,750);
+            bitBuffer.writeInt(422,350,750);
+            bitBuffer.writeInt(422,350,750);
+            bitBuffer.flush();
+            sending_socket.SendTo(bitBuffer.getBuffer(), sending_end_point);
         }
         catch (Exception socketException)
         {
             Debug.Log("Socket exception: " + socketException);
         }
     }
+
 }
