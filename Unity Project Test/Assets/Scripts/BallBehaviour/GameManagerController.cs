0523ba0a6438de1235c3826638728ed23026aa8d
axelfratoni <afratoni@itba.edu.ar>
2018-08-30 23:25:15 -0300

    Main menu added to roll-a-ball

---
+++ Unity Project Test/Assets/Scripts/BallBehaviour/GameManagerController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerController : MonoBehaviour {
+
+    private UDPChannel udpChannel;
+    public int localPort = 11001;
+
+	public void Initialize(object[] messageContent)
+    {
+        string targetIP = (string) messageContent[0];
+        int targetPort = (int) messageContent[1];
+        try
+        {
+            udpChannel = new UDPChannel(targetIP, targetPort, localPort, ReceiveAction);
+            GameObject.Find("Menu").SetActive(false);
+        }
+        catch(Exception e)
+        {
+            Debug.Log("Connection error");
+        }
+    }
+
+    private void ReceiveAction(UDPChannel udpChannel, Byte[] receivedBytes)
+    {
+        Debug.Log("Received.");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnDisable()
+    {
+        udpChannel.Disable();
+    }
+}
---
+++ Unity Project Test/Assets/Scripts/MainMenu/ButtonController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ButtonController : MonoBehaviour{
+
+    private Button submitButton;
+    private GameObject gameManager;
+
+    public InputField targetIP;
+    public InputField targetPort;
+
+    private void Start()
+    {
+        submitButton = GetComponent<Button>();
+        gameManager = GameObject.Find("GameManager");
+        submitButton.onClick.AddListener(HandleClick);
+    }
+
+    private void HandleClick()
+    {
+        object[] messageContent = new object[2];
+        messageContent[0] = targetIP.text;
+        messageContent[1] = Int32.Parse(targetPort.text);
+        gameManager.SendMessage("Initialize", messageContent);
+    }
+}
