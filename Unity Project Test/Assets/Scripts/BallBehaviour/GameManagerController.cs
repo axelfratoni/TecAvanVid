0ca8cb1c783c2808003d65463a2af9a1053cca0c
axelfratoni <afratoni@itba.edu.ar>
2018-08-31 00:03:05 -0300

    Added server-client toggler

--- Unity Project Test/Assets/Scripts/BallBehaviour/GameManagerController.cs
+++ Unity Project Test/Assets/Scripts/BallBehaviour/GameManagerController.cs
@@ -8,10 +8,26 @@ public class GameManagerController : MonoBehaviour {
     private UDPChannel udpChannel;
     public int localPort = 11001;
 
-	public void Initialize(object[] messageContent)
+    public void InitializeServer(object[] messageContent)
+    {
+        int targetPort = (int)messageContent[0];
+        Debug.Log("Server");
+        try
+        {
+            udpChannel = new UDPChannel(targetPort, ReceiveAction);
+            GameObject.Find("Menu").SetActive(false);
+        }
+        catch (Exception e)
+        {
+            Debug.Log("Connection error");
+        }
+    }
+
+    public void InitializeClient(object[] messageContent)
     {
         string targetIP = (string) messageContent[0];
         int targetPort = (int) messageContent[1];
+        Debug.Log("Client");
         try
         {
             udpChannel = new UDPChannel(targetIP, targetPort, localPort, ReceiveAction);
--- Unity Project Test/Assets/Scripts/MainMenu/ButtonController.cs
+++ Unity Project Test/Assets/Scripts/MainMenu/ButtonController.cs
@@ -10,6 +10,7 @@ public class ButtonController : MonoBehaviour{
 
     public InputField targetIP;
     public InputField targetPort;
+    public InputField hostingPort;
 
     private void Start()
     {
@@ -20,9 +21,19 @@ public class ButtonController : MonoBehaviour{
 
     private void HandleClick()
     {
-        object[] messageContent = new object[2];
-        messageContent[0] = targetIP.text;
-        messageContent[1] = Int32.Parse(targetPort.text);
-        gameManager.SendMessage("Initialize", messageContent);
+        if(hostingPort.isActiveAndEnabled)
+        {
+            object[] messageContent = new object[1];
+            messageContent[0] = Int32.Parse(hostingPort.text);
+            gameManager.SendMessage("InitializeServer", messageContent);
+        }
+        else
+        {
+            object[] messageContent = new object[2];
+            messageContent[0] = targetIP.text;
+            messageContent[1] = Int32.Parse(targetPort.text);
+            gameManager.SendMessage("InitializeClient", messageContent);
+        }
+
     }
 }
