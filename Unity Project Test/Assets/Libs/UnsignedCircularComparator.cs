4b29a436d18589aeadecf3e491cf43584b9d91cc
EricHorvat <eric.nahuel.horvat@gmail.com>
2018-10-07 16:35:41 -0300

    [FIX] From ulong to n bits

--- Unity Project Test/Assets/Libs/UnsignedCircularComparator.cs
+++ Unity Project Test/Assets/Libs/UnsignedCircularComparator.cs
@@ -1,24 +1,30 @@
+using System;
+
 namespace Libs
 {
     public class UnsignedCircularComparator
     {
-        public static int compareLong(ulong a, ulong b)
+        public static int compareLong(ulong a, ulong b, uint bits)
         {
             if (a != b)
             {
+                ulong module = (ulong)Math.Pow(2,bits);
+                ulong max = module - 1;
+                ulong bitMostSignificant = (max - (max >> 1) );
                 /* Eg1: a = 25 b = 245, con 255 como maximo (2**8 - 1); b es menor en lo circular
                  * Eg2: a = 25 b = 35, con 255 como maximo (2**8 - 1); b es mayor en lo circular
                  * Eg2: a = 25 b = 153, con 255 como maximo (2**8 - 1); b es el opuesto en lo circular
                  * Lo "llevo" a comparar con 0
                  * */
-                ulong diff = b - a; // 220 = (base2) 11011100 // 10 = (base2) 00001010 // 128 (base2) 10000000
+                ulong diff = (b - a) % module; // 220 = (base2) 11011100 // 10 = (base2) 00001010 // 128 (base2) 10000000
                 /* Me importa el bit mas significativo
                  * Por lo que 11111111 - 01111111 = 10000000
                  * */
-                if ((diff & (ulong.MaxValue - ulong.MaxValue >> 1 )) != 0)
+                
+                if ((diff & bitMostSignificant) != 0)
                 {
                     //En el caso opuesto en lo circular entra aca, por lo que hay que ser consistentes
-                    if (diff == (ulong.MaxValue - ulong.MaxValue >> 1))
+                    if (diff == bitMostSignificant)
                     {
                         return a > b ? 1 : -1;
                     }
